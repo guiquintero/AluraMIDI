08c574923964bc026b85d46f17e210c0f6d341c1
Quintero <guiquintero02@gmail.com>
2023-03-23 08:54:22 -0300

    Todos os botoes ja funcionam

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play()
-    
+function tocaSom(idAudio) {
+    document.querySelector(idAudio).play()
 }
 
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play()
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador <listaDeTeclas.length){
+    
+    const tecla = listaDeTeclas[contador];
+
+    const instrumeto = tecla.classList[1];
+
+    const idAudio = `#som_${instrumeto}`;
+    
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
 }
